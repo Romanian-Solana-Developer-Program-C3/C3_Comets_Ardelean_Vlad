24389e9859520b303d98061659da29e29b1531c9
Vlad Ardelean <vladardelean000@gmail.com>
2025-04-01 23:15:22 +0300

    Lab5 fixed

--- lab5/favorites/programs/favorites/src/lib.rs
+++ lab5/favorites/programs/favorites/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(unexpected_cfgs)]
 use anchor_lang::prelude::*;
 
 declare_id!("8u4S6a4jRam2kAJjQmdGn2fLVxUZfKdm7VqkxHgV6emf");
@@ -10,7 +11,50 @@ pub mod favorites {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn set_favorites(
+        ctx: Context<SetFavorites>, 
+        color: String, 
+        number: u64, 
+        hobbies: Vec<String>
+    ) -> Result<()> {
+        msg!("Setting favorites for: {:?}", ctx.accounts.user.key());
+        
+        ctx.accounts.favorites.set_inner(Favorites {
+            color,
+            number,
+            hobbies,
+        });
+
+        msg!("Favorites set successfully!");
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct SetFavorites<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init,
+        payer = user,
+        seeds = [b"favorites", user.key().as_ref()],
+        space = 8 + Favorites::INIT_SPACE,
+        bump
+    )]
+    pub favorites: Account<'info, Favorites>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    #[max_len(50)]
+    pub color: String,
+    pub number: u64,
+    #[max_len(5, 50)]
+    pub hobbies: Vec<String>,
+}
