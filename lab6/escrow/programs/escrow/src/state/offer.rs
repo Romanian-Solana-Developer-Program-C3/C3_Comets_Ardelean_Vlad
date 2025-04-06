c8ef22f395b81f67322db2a853a802b63aad135d
Vlad Ardelean <vladardelean000@gmail.com>
2025-04-06 21:11:45 +0300

    Course 8 Escrow

--- lab6/escrow/programs/escrow/src/state/offer.rs
+++ lab6/escrow/programs/escrow/src/state/offer.rs
@@ -2,12 +2,12 @@ use anchor_lang::prelude::*;
 
 #[account]
 #[derive(InitSpace)]
-pub struct Offer{
-    pub id:u64,
-pub maker:Pubkey,
-pub token_b_wanted_amount:u64,
-pub token_b_mint:Pubkey,
-pub token_a_amount:u64,
-pub token_a_mint:Pubkey,
-
+pub struct Offer {
+    pub id: u64,
+    pub maker: Pubkey,
+    pub token_a_amount: u64,
+    pub token_mint_a: Pubkey,
+    pub token_b_wanted_amount: u64,
+    pub token_mint_b: Pubkey,
+    pub bump: u8,
 }
